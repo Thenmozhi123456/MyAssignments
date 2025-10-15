3c6decefdd6ed134ee88cfe490736be0447d017c
Thenmozhi123456 <thendmt06@gmail.com>
2025-10-15 21:19:45 +0530

    Add files via upload
    day1

---
+++ week1_day1/week1_day1.js
@@ -0,0 +1,19 @@
+console.log("Welcome")
+
+var FirstName = "Thenmozhi";
+var companyName = "Maveric";
+var mobileNumber = 2345678912;
+var isAutomation = true;
+var hasPlaywright;
+
+console.log("FirstName:",typeof(FirstName));
+console.log("companyName:",typeof(companyName));
+console.log("mobileNumber:",typeof(mobileNumber));
+console.log("isAutomation:",typeof(isAutomation));
+console.log("hasPlaywright:",typeof(hasPlaywright));
+
+console.log("FirstName:" + FirstName + ":" + typeof(FirstName));
+console.log("companyName:" + companyName + ":" + typeof(companyName));
+console.log("mobileNumber::" +mobileNumber +":" + typeof(mobileNumber));
+console.log("isAutomation:" + isAutomation +":" + typeof(isAutomation));
+console.log("hasPlaywright:" + hasPlaywright + ":" + typeof(hasPlaywright));
