1b7d931ca24846a4759fae9eb6222f107eb729cf
Thenmozhi123456 <thendmt06@gmail.com>
2025-10-16 12:26:23 +0530

    Add files via upload
    Day 2

---
+++ week1_day2.js
@@ -0,0 +1,14 @@
+
+var empDetails = "Thenu"
+empDetails = 2025
+var empDetails = true
+console.log(empDetails)
+
+let studentInfo = 2025
+studentInfo = null
+console.log(studentInfo)
+// re declaration not possible
+
+const piValue = 3.14
+console.log(piValue)
+// re declaration and re assignment not possible
---
+++ week1_day2_ifCond.js
@@ -0,0 +1,13 @@
+let mark = 80
+if (mark>70){
+    console.log("A Grade :"+ mark)
+}
+else if (mark>55){
+    console.log("B Grade :" + mark)
+}
+else if (mark>35){
+    console.log("C Grade :" + mark)
+}
+ else{
+    console.log ("D Grade :" + mark)
+ }
